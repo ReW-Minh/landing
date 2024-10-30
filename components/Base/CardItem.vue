68ab4d6a128cdb4e00bc091b07862405b19d20bc
Ha Minh <minhha@viajsc.onmircrosoft.com>
2024-10-30 23:54:28 +0700

    Update: slate services 2.0

--- components/Base/CardItem.vue
+++ components/Base/CardItem.vue
@@ -1,79 +1,91 @@
 <template>
-    <Card class="bio-card w-full h-full overflow-hidden relative" :class="{ 'card-clickable': hoverEffect }">
-        <template #header>
-            <div class="w-full overflow-hidden" :class="squareImage ? 'aspect-square' : 'h-[280px]'">
-                <slot name="image" />
-            </div>
-        </template>
-        <template #title>
-            <slot name="title" />
-        </template>
-        <template #subtitle>
-            <slot name="subtitle" />
-        </template>
-    </Card>
+  <Card class="bio-card w-full h-full overflow-hidden relative" :class="{ 'card-clickable': hoverEffect }">
+    <template #header>
+      <div class="w-full overflow-hidden" :class="imageHeight">
+        <slot name="image"/>
+      </div>
+    </template>
+    <template #title>
+      <slot name="title"/>
+    </template>
+    <template #subtitle>
+      <slot name="subtitle"/>
+    </template>
+  </Card>
 </template>
 
 <script setup>
-defineProps({
-    hoverEffect: {
-        type: Boolean,
-        default: true
-    },
-    squareImage: {
-        type: Boolean,
-        default: false
-    }
+const props = defineProps({
+  hoverEffect: {
+    type: Boolean,
+    default: true
+  },
+  squareImage: {
+    type: Boolean,
+    default: false
+  },
+  customHeight: {
+    type: String,
+    default: ''
+  }
+})
+
+const imageHeight = computed(() => {
+  if (props.customHeight) {
+    return `h-[${props.customHeight}]`
+  }
+
+  return props.squareImage ? 'aspect-square' : 'h-[280px]'
 })
 </script>
 
 <style lang="scss">
 .bio-card {
-    .bio-picture {
-        object-fit: cover;
-        height: 100%;
-        width: 100%;
-        transition: all ease-in-out .2s;
+  .bio-picture {
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+    transition: all ease-in-out .2s;
+  }
+
+  .hover {
+    inset: 0;
+    background-color: rgba(0, 0, 0, .5);
+    opacity: 0;
+    transition: all linear .1s;
+    --p-icon-size: 30px;
+    --p-button-text-primary-color: white
+  }
+
+  .p-button-text {
+    transition: all linear .1s;
+  }
+
+  .p-button-text:not(:disabled):hover {
+    background-color: transparent;
+    scale: 1.2;
+  }
+
+  &.card-hover {
+    &:hover {
+      .hover {
+        opacity: 1;
+      }
     }
+  }
 
-    .hover {
-        inset: 0;
-        background-color: rgba(0, 0, 0, .5);
-        opacity: 0;
-        transition: all linear .1s;
-        --p-icon-size: 30px;
-        --p-button-text-primary-color: white
+  &.card-clickable {
+    cursor: pointer;
+    transition: all ease-in-out .2s;
+
+    &:hover {
+      transform: translateY(-5px);
+      box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+
+      .bio-picture {
+        transform: scale(1.05)
+      }
     }
-
-    .p-button-text {
-        transition: all linear .1s;
-    }
-
-    .p-button-text:not(:disabled):hover {
-        background-color: transparent;
-        scale: 1.2;
-    }
-
-    &.card-hover {
-        &:hover {
-            .hover {
-                opacity: 1;
-            }
-        }
-    }
-
-    &.card-clickable {
-        cursor: pointer;
-        transition: all ease-in-out .2s;
-
-        &:hover {
-            transform: translateY(-5px);
-            box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-
-            .bio-picture {
-                transform: scale(1.05)
-            }
-        }
-    }
+  }
 }
 </style>
--- pages/services/slate/index.vue
+++ pages/services/slate/index.vue
@@ -1,104 +1,182 @@
 <template>
-    <BasePageContainer>
-        <img src="/img/logo-bg.svg" alt="rew"
-            class="bg-logo !top-1/2 !left-1/2 !transform -translate-x-1/2 !-translate-y-1/2 hidden md:block mt-[150px]">
+  <BasePageContainer>
+    <img src="/img/logo-bg.svg" alt="rew"
+         class="bg-logo !top-1/2 !left-1/2 !transform -translate-x-1/2 !-translate-y-1/2 hidden md:block mt-[150px]">
 
-        <BasePageHeader>
-            <template #default>
-                <span class="rew-text-green">Slaters</span> at Your Service
-            </template>
-        </BasePageHeader>
+    <BasePageHeader>
+      <template #default>
+        <span class="rew-text-green">Slaters</span> at Your Service
+      </template>
+    </BasePageHeader>
 
-        <BasePageContent>
-            <section>
-                <div class="mb-3 text-3xl font-semibold rew-text-brown">
-                    Free <span class="rew-text-green rew-font">Re</span>Sources
-                </div>
-                <div class=" grid grid-cols-1 md:grid-cols-3 gap-12">
-                    <div v-for="item in resources" :key="item.id">
-                        <NuxtLink :to="item.link" :target="item.type === CONTACT_TYPE.PODCAST ? '_self' : '_blank'">
-                            <BaseCardItem class="resource-card">
-                                <template #image>
-                                    <div class="resource-icon grid place-items-center h-full">
-                                        <IconReSource v-if="item.type === CONTACT_TYPE.RESOURCE" />
-                                        <IconPresentations
-                                            v-if="item.type === CONTACT_TYPE.PRESENTATIONS_PUBLICATIONS" />
-                                        <IconPodcast v-if="item.type === CONTACT_TYPE.PODCAST" />
-                                    </div>
-                                </template>
-                                <template #title>
-                                    <div v-html="item.title"></div>
-                                </template>
-                            </BaseCardItem>
-                        </NuxtLink>
+    <BasePageContent>
+      <section>
+        <div class="mb-3 text-3xl font-semibold rew-text-brown">
+          Free <span class="rew-text-green rew-font">Re</span>Sources
+        </div>
+        <div class=" grid grid-cols-1 md:grid-cols-3 gap-12">
+          <div v-for="item in resources" :key="item.id">
+            <NuxtLink :to="item.link" :target="item.type === CONTACT_TYPE.PODCAST ? '_self' : '_blank'">
+              <BaseCardItem class="resource-card">
+                <template #image>
+                  <div class="resource-icon grid place-items-center h-full">
+                    <IconReSource v-if="item.type === CONTACT_TYPE.RESOURCE"/>
+                    <IconPresentations
+                        v-if="item.type === CONTACT_TYPE.PRESENTATIONS_PUBLICATIONS"/>
+                    <IconPodcast v-if="item.type === CONTACT_TYPE.PODCAST"/>
+                  </div>
+                </template>
+                <template #title>
+                  <div v-html="item.title"></div>
+                </template>
+              </BaseCardItem>
+            </NuxtLink>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div class="mb-3 text-3xl font-semibold rew-text-brown">
+          Flexible, transparent pricing for every <span class="slate-text-blue">Slate School</span>
+        </div>
+        <!--<HomeServices />-->
+        <div class=" grid grid-cols-1 md:grid-cols-2 gap-12">
+          <div>
+            <BaseCardItem :hoverEffect="false" customHeight="380px"
+                          class="resource-card !cursor-default !bg-white/50 !backdrop-blur-sm">
+              <template #image>
+                <div class="flex items-center m-6 h-full">
+                  <div class="font-bold rew-text-brown text-2xl space-y-2">
+                    <div>
+                      Pay As You Go
                     </div>
+                    <div class="font-bold rew-text-green text-2xl">
+                      $285/hour
+                    </div>
+                    <div class="p-card-subtitle italic text-sm font-normal">
+                      Post-pay monthly only for what you utilize.
+                      Best for Slate teams that have operations in place and who are looking for the security of direct
+                      access to extra Slater <span class="rew-font"><span class="rew-text-green">Re</span>Sources</span>
+                      when urgencies arise.
+                    </div>
+                  </div>
                 </div>
-            </section>
-
-            <section>
-                <div class="mb-3 text-3xl font-semibold rew-text-brown">
-                    Slate Support and Staffing Solutions
+              </template>
+              <template #title>
+                <div class="flex justify-center">
+                  <CallToAction/>
                 </div>
-                <HomeServices />
-            </section>
+              </template>
+            </BaseCardItem>
+          </div>
 
-            <section>
-                <HomeFAQ />
-            </section>
-        </BasePageContent>
-    </BasePageContainer>
+          <div>
+            <BaseCardItem :hoverEffect="false" customHeight="380px"
+                          class="resource-card !cursor-default !bg-white/50 !backdrop-blur-sm">
+              <template #image>
+                <div class="flex items-center m-6 h-full">
+                  <div class="font-bold rew-text-brown text-2xl space-y-2">
+                    <div>
+                      Bundles
+                    </div>
+                    <div class="font-bold rew-text-green text-2xl">
+                      <div><span class="rew-text-brown">20 hrs:</span> $275/hour</div>
+                      <div><span class="rew-text-brown">30 hrs:</span> $265/hour</div>
+                      <div><span class="rew-text-brown">40 hrs:</span> $250/hour</div>
+                      <div><span class="rew-text-brown">41+ Hours?</span> Contact us for a custom discounted quote.
+                      </div>
+                    </div>
+                    <div class="p-card-subtitle italic text-sm font-normal">
+                      Save with purchased hours in advance to use as needed. Best for Slate teams seeking greater
+                      operations efficiencies, staff gap fills, implementations, and enhancement projects.
+                    </div>
+                  </div>
+                </div>
+              </template>
+              <template #title>
+                <div class="flex justify-center">
+                  <CallToAction/>
+                </div>
+              </template>
+            </BaseCardItem>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <!--<HomeFAQ />-->
+        <ul class="flex flex-wrap gap-x-4 w-full justify-center text-xl font-semibold rew-text-brown list-inside list-checkmark">
+          <li>Rapid Onboarding</li>
+          <li>Unrestricted Support</li>
+          <li>Transparency</li>
+          <li>No Long-Term Commitments Required</li>
+        </ul>
+
+        <div class="flex justify-center mt-8">
+
+          <div class="border rounded-full shadow !bg-white/50 !backdrop-blur-sm hover:!shadow-md transition-all">
+            <RouterLink
+                class="text-3xl font-semibold text-gradient-animation block px-6 py-2"
+                to="/services/how-we-work">
+              See How We Work
+            </RouterLink>
+          </div>
+        </div>
+      </section>
+    </BasePageContent>
+  </BasePageContainer>
 </template>
 
 <script setup>
 useHead({
-    title: 'ReWorkflow - Slate',
+  title: 'ReWorkflow - Slate',
 })
 
 definePageMeta({
-    middleware: ['hidden']
+  middleware: ['hidden']
 })
 
 const resources = [
-    {
-        type: CONTACT_TYPE.RESOURCE,
-        title: `<span class="rew-text-green rew-font">Re</span>Source Articles`,
-        link: SLATE_LINK,
-    },
-    {
-        type: CONTACT_TYPE.PRESENTATIONS_PUBLICATIONS,
-        title: `Presentations`,
-        link: 'https://resource.reworkflow.com/search?term=%5BPresentations%3DSlate%5D'
-    },
-    {
-        type: CONTACT_TYPE.PODCAST,
-        title: `Podcast`,
-        link: '/podcast'
-    }
+  {
+    type: CONTACT_TYPE.RESOURCE,
+    title: `<span class="rew-text-green rew-font">Re</span>Source Articles`,
+    link: SLATE_LINK,
+  },
+  {
+    type: CONTACT_TYPE.PRESENTATIONS_PUBLICATIONS,
+    title: `Presentations`,
+    link: 'https://resource.reworkflow.com/search?term=%5BPresentations%3DSlate%5D'
+  },
+  {
+    type: CONTACT_TYPE.PODCAST,
+    title: `Podcast`,
+    link: '/podcast'
+  }
 ]
 
 const services = [
-    {
-        route: '/services/powercampus/power-slate',
-        title: `Slate
+  {
+    route: '/services/powercampus/power-slate',
+    title: `Slate
                 <br />
                 Integration
                 <br />
                 (PowerSlate)`
-    },
-    {
-        route: '/services/powercampus/version-upgrades',
-        title: `Version
+  },
+  {
+    route: '/services/powercampus/version-upgrades',
+    title: `Version
                 <br />
                 Upgrades`
-    },
-    {
-        route: '/services/powercampus/powerfaids',
-        title: `PowerFAIDS
+  },
+  {
+    route: '/services/powercampus/powerfaids',
+    title: `PowerFAIDS
                 <br />
                 Integration
                 <br />
                 Add-On`
-    }
+  }
 ]
 </script>
 
@@ -141,5 +219,30 @@ const services = [
             }
         }
     }
+}
+
+.text-gradient-animation {
+  background: linear-gradient(
+          to right,
+          #486C3C 20%,
+          #768471 30%,
+          #8F4A2C 70%,
+          #433129 80%
+  );
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  text-fill-color: transparent;
+  background-size: 500% auto;
+  animation: textShine 5s ease-in-out infinite alternate;
+}
+
+@keyframes textShine {
+  0% {
+    background-position: 0% 50%;
+  }
+  100% {
+    background-position: 100% 50%;
+  }
 }
 </style>
