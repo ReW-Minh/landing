23966e58b42dd10b4426e8b13750578e91a0eb19
Ha Minh <minhha@viajsc.onmircrosoft.com>
2024-10-31 10:26:13 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <label class="!fixed z-10 right-4 top-[120px] cursor-pointer flex items-center gap-1 select-none rounded-xl bg-white/50 backdrop-blur-sm py-1 px-2 rew-text-brown">
+  <label class="!fixed z-10 right-4 top-[60px] md:top-[120px] cursor-pointer flex items-center gap-1 select-none rounded-xl bg-white/50 backdrop-blur-sm py-1 px-2 rew-text-brown">
     <Checkbox v-model="visible" :binary="true"/>
     <span>Show Services</span>
   </label>
