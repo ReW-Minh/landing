7ec598c314265d6fcb76b494597373d05f7409f6
Ha Minh <minhha@viajsc.onmircrosoft.com>
2024-11-24 21:01:00 +0700

    update services/slate

--- components/Home/Services.vue
+++ components/Home/Services.vue
@@ -1,73 +1,34 @@
 <template>
   <section class="pt-4 md:pb-10 pb-6 overflow-hidden" id="service">
-    <img src="/img/logo-bg.svg" alt="rew" class="bg-logo invisible md:visible">
-    <div class="max-w-[960px] mx-auto invisible md:visible px-4">
-      <div class="grid grid-cols-2 gap-10">
+    <div class="max-w-[960px] mx-auto px-4">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10">
         <div v-for="item in slaterRoles" class="border rounded-xl bg-white/30 backdrop-blur-sm shadow">
           <div class="card !my-6 !mx-4">
-            <div class="card-img">
-              <img :src="item.imgSrc" :alt="item.title" class="mx-auto">
-            </div>
             <div class="card-title nowrap" v-html="item.title"></div>
             <div class="card-content" v-html="decorateText(item.content)"></div>
           </div>
         </div>
       </div>
     </div>
-    <div class="block md:hidden">
-      <swiper-container slides-per-view="1.4" centered-slides="true" space-between="7">
-        <swiper-slide v-for="item in slaterRoles">
-          <div class="card">
-            <div class="card-img">
-              <img :src="item.imgSrcMobile" :alt="item.title">
-            </div>
-            <div class="card-title" v-html="item.title">
-            </div>
-            <div class="card-content" v-html="decorateText(item.content)"></div>
-          </div>
-        </swiper-slide>
-      </swiper-container>
-    </div>
   </section>
 </template>
 
 <script setup>
-import { register } from 'swiper/element/bundle'
-
-import role01Url from '/img/role01.svg'
-import role01MobileUrl from '/img/role01.webp'
-import role02Url from '/img/role02.svg'
-import role02MobileUrl from '/img/role02.webp'
-import role03Url from '/img/role03.svg'
-import role03MobileUrl from '/img/role03.webp'
-import role04Url from '/img/role04.svg'
-import role04MobileUrl from '/img/role04.webp'
-
-register()
-
 const slaterRoles = ref([
   {
-    imgSrc: role01Url,
-    imgSrcMobile: role01MobileUrl,
     title: 'Captaining',
     content: 'Team up with our Slate subject matter experts for embedded project management, cycle prep, and strategic advisement.'
   },
   {
-    imgSrc: role02Url,
-    imgSrcMobile: role02MobileUrl,
     title: 'Engineering',
-    content: 'Let’s achieve big together for implementing next-level Slate enhancements – from portals and dashboards to workflow automations and system integrations.'
+    content: 'Let’s achieve big together with next-level Slate enhancements – from portals and dashboards to workflow automations and system integrations.'
   },
   {
-    imgSrc: role03Url,
-    imgSrcMobile: role03MobileUrl,
     title: 'Operationalizing',
     content: 'Troubleshooting, routine maintenance, querying, reporting, data integrity – you name it! ReW’s operations specialists take on daily tasks so that your team is free to focus on student-facing priorities.'
   },
   {
-    imgSrc: role04Url,
-    imgSrcMobile: role04MobileUrl,
-    title: 'ReInforcing',
+    title: 'Reinforcing',
     content: 'Looking for Slate-skilled, seasonal application readers or processors? Coverage for a key Slater going on leave? Need to train up your in-house talent? Call on ReW to bridge the gap.'
   },
 ])
--- pages/services/slate/index.vue
+++ pages/services/slate/index.vue
@@ -10,104 +10,9 @@
     </BasePageHeader>
 
     <BasePageContent>
-      <section>
-        <div class="mb-3 text-3xl font-semibold rew-text-brown">
-          Free <span class="rew-text-green rew-font">Re</span>Sources
-        </div>
-        <div class=" grid grid-cols-1 md:grid-cols-3 gap-12">
-          <div v-for="item in resources" :key="item.id">
-            <NuxtLink :to="item.link" :target="item.type === CONTACT_TYPE.PODCAST ? '_self' : '_blank'">
-              <BaseCardItem class="resource-card new-tab-hover">
-                <template #image>
-                  <div class="resource-icon grid place-items-center h-full">
-                    <IconReSource v-if="item.type === CONTACT_TYPE.RESOURCE"/>
-                    <IconPresentations
-                        v-if="item.type === CONTACT_TYPE.PRESENTATIONS_PUBLICATIONS"/>
-                    <IconPodcast v-if="item.type === CONTACT_TYPE.PODCAST"/>
-                  </div>
-                </template>
-                <template #title>
-                  <div class="flex items-center">
-                    <div v-html="item.title"></div>
-                    <IconExternal v-if="item.type !== CONTACT_TYPE.PODCAST" />
-                  </div>
-                </template>
-              </BaseCardItem>
-            </NuxtLink>
-          </div>
-        </div>
-      </section>
+      <HomeServices/>
 
       <section>
-        <div class="mb-3 text-3xl font-semibold rew-text-brown">
-          Flexible, transparent pricing for every <span class="slate-text-blue">Slate School</span>
-        </div>
-        <!--<HomeServices />-->
-        <div class=" grid grid-cols-1 md:grid-cols-2 gap-12">
-          <div>
-            <BaseCardItem :hoverEffect="false" :customHeight="true"
-                          class="resource-card !cursor-default !bg-white/30 !backdrop-blur-sm">
-              <template #image>
-                <div class="flex items-center m-6 h-full">
-                  <div class="font-bold rew-text-brown text-2xl space-y-2">
-                    <div>
-                      Pay As You Go
-                    </div>
-                    <div class="font-bold rew-text-green text-2xl">
-                      $285/hour
-                    </div>
-                    <div class="p-card-subtitle italic text-sm font-normal">
-                      Post-pay monthly only for what you utilize.
-                      Best for Slate teams that have operations in place and who are looking for the security of direct
-                      access to extra Slater <span class="rew-font"><span class="rew-text-green">Re</span>Sources</span>
-                      when urgencies arise.
-                    </div>
-                  </div>
-                </div>
-              </template>
-              <template #title>
-                <div class="flex justify-center">
-                  <CallToAction/>
-                </div>
-              </template>
-            </BaseCardItem>
-          </div>
-
-          <div>
-            <BaseCardItem :hoverEffect="false" :customHeight="true"
-                          class="resource-card !cursor-default !bg-white/30 !backdrop-blur-sm">
-              <template #image>
-                <div class="flex items-center m-6 h-full">
-                  <div class="font-bold rew-text-brown text-2xl space-y-2">
-                    <div>
-                      Bundles
-                    </div>
-                    <div class="font-bold rew-text-green text-2xl">
-                      <div><span class="rew-text-brown">20 hrs:</span> $275/hour</div>
-                      <div><span class="rew-text-brown">30 hrs:</span> $265/hour</div>
-                      <div><span class="rew-text-brown">40 hrs:</span> $250/hour</div>
-                      <div><span class="rew-text-brown">41+ Hours?</span> Contact us for a custom discounted quote.
-                      </div>
-                    </div>
-                    <div class="p-card-subtitle italic text-sm font-normal">
-                      Save with purchased hours in advance to use as needed. Best for Slate teams seeking greater
-                      operations efficiencies, staff gap fills, implementations, and enhancement projects.
-                    </div>
-                  </div>
-                </div>
-              </template>
-              <template #title>
-                <div class="flex justify-center">
-                  <CallToAction/>
-                </div>
-              </template>
-            </BaseCardItem>
-          </div>
-        </div>
-      </section>
-
-      <section>
-        <!--<HomeFAQ />-->
         <ul class="flex flex-wrap gap-x-4 w-full justify-center text-xl font-semibold rew-text-brown list-inside list-checkmark">
           <li>Rapid Onboarding</li>
           <li>Unrestricted Support</li>
@@ -116,12 +21,11 @@
         </ul>
 
         <div class="flex justify-center mt-8">
-
           <div class="border rounded-full shadow !bg-white/30 !backdrop-blur-sm hover:!shadow-md transition-all">
             <RouterLink
                 class="text-3xl font-semibold text-gradient-animation block px-6 py-2"
-                to="/services/how-we-work">
-              See How We Work
+                to="/pricing">
+              Pricing
             </RouterLink>
           </div>
         </div>
@@ -156,43 +60,43 @@ const resources = [
 
 <style scoped lang="scss">
 .resource-card {
+  .text-heading {
+    transition: all ease-in-out .2s;
+    background-color: var(--rew-primary-green);
+  }
+
+  &:hover {
+    .resource-icon {
+      background-color: var(--rew-primary-brown);
+
+      svg {
+        transform: scale(1.05);
+      }
+    }
+
     .text-heading {
-        transition: all ease-in-out .2s;
-        background-color: var(--rew-primary-green);
+      @apply text-[1.9rem];
+
+      background-color: var(--rew-primary-brown);
     }
-
-    &:hover {
-        .resource-icon {
-            background-color: var(--rew-primary-brown);
-
-            svg {
-                transform: scale(1.05);
-            }
-        }
-
-        .text-heading {
-            @apply text-[1.9rem];
-
-            background-color: var(--rew-primary-brown);
-        }
-    }
+  }
 }
 
 .resource-icon {
+  transition: all ease-in-out .2s;
+  background-color: var(--rew-primary-green);
+
+  svg {
     transition: all ease-in-out .2s;
-    background-color: var(--rew-primary-green);
+    height: 170px;
+    width: 220px;
 
-    svg {
-        transition: all ease-in-out .2s;
-        height: 170px;
-        width: 220px;
-
-        &:not(.resource) {
-            * {
-                fill: white;
-            }
-        }
+    &:not(.resource) {
+      * {
+        fill: white;
+      }
     }
+  }
 }
 
 .text-gradient-animation {
