8ff69a3662849efc4a1b4202189a4147e3491e21
Ha Minh <minhha@viajsc.onmircrosoft.com>
2024-11-27 12:37:29 +0700

    add CTA to home page

--- components/CallToAction.vue
+++ components/CallToAction.vue
@@ -1,5 +1,5 @@
 <template>
-  <NuxtLink :to="link ?? '/contact'" :target="link ? '_blank' : '_self'" :title="label ?? 'Contact us'"
+  <NuxtLink :to="link ?? '/contact'" :target :title="label ?? 'Contact us'"
             class="rew-main-btn rew-box-shadow inline-flex items-center"
             :class="{ 'h-10 !text-base': size === 'md', 'footer': footer }">
     <span>{{ label ?? 'Contact' }}</span>
@@ -7,7 +7,7 @@
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
   label: {
     type: String,
   },
@@ -20,6 +20,16 @@ defineProps({
   },
   footer: {
     type: Boolean
+  },
+  internal: {
+    type: Boolean,
   }
+})
+
+const target = computed(() => {
+  if (props.internal)
+    return '_self'
+
+  return props.link ? '_blank' : '_self'
 })
 </script>
--- components/Home/Index.vue
+++ components/Home/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="pt-3 pb-6 md:overflow-hidden overflow-visible" id="home">
+  <section class="pt-5 md:pt-8 pb-6 md:overflow-hidden overflow-visible" id="home">
     <div class="container-fluid">
       <div class="grid grid-cols-12 gap-3">
         <div class="lg:col-span-9 md:col-span-8 col-span-12">
@@ -8,7 +8,7 @@
                  class="bg-logo -left-[24px] !top-1/2 !transform !-translate-y-1/2 !opacity-5 invisible md:visible">
 
             <div data-aos="fade-right" data-aos-duration="700"
-                 class="left-home-block position-relative mt-6 md:mt-12 relative">
+                 class="left-home-block position-relative mt-6 md:mt-0 relative">
               <h1 class="hero-title rew-text-green leading-tight">
                 Slate Support and Staffing Solutions
               </h1>
@@ -20,6 +20,10 @@
                 We offer flexibly structured services that fit seamlessly with your institution’s needs. Our lean team
                 of experts provides quick, high-quality results for all Slate issues and implementation projects.
               </div>
+            </div>
+
+            <div class=" mt-4 md:mt-6 lg:mt-10">
+              <CallToAction label="Slate Services" link="/services/slate" size="lg" :internal="true" />
             </div>
           </div>
         </div>
